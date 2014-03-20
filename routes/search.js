70ad6064ba17f13995c35d00cfd489f792e8354d
Kurt Symanzik <kurt_symanzik@sil.org>
2014-03-20 20:26:31 +0800

    Added a simple form of pagination to the search

--- routes/search.js
+++ routes/search.js
@@ -11,6 +11,7 @@ var _ = require('underscore')
   , Promise = require('bluebird')
   , Pregnancy = require('../models').Pregnancy
   , Pregnancies = require('../models').Pregnancies
+  , cfg = require('../config')
   , logInfo = require('../util').logInfo
   , logWarn = require('../util').logWarn
   , logError = require('../util').logError
@@ -36,7 +37,9 @@ var view = function(req, res) {
  * return
  * -------------------------------------------------------- */
 var execute = function(req, res) {
-  var flds = _.omit(req.body, ['_csrf'])
+  var flds = _.omit(req.body, ['_csrf', 'searchType'])
+    , pageNum = 1
+    , rowsPerPage = parseInt(cfg.search.rowsPerPage, 10)
     , qb
     results = []
     , cols = [
@@ -52,6 +55,21 @@ var execute = function(req, res) {
     , lnOp = '='
     , otherOp = '='
     ;
+
+  // --------------------------------------------------------
+  // Store the search criteria in the session so that it is
+  // there if pagination is needed.
+  // --------------------------------------------------------
+  if (req.body.searchType === 'new') {
+    req.session.searchFlds = flds;
+    req.session.searchPage = pageNum;
+    req.session.save();
+  } else {
+    pageNum = req.session.searchPage = req.session.searchPage + 1;
+    flds = req.session.searchFlds;
+    req.session.save();
+  }
+
   qb = new Pregnancy().query();
   qb.join('patient', 'patient.id', '=', 'pregnancy.patient_id');
   if (flds.firstname && flds.firstname.length > 0) {
@@ -69,7 +87,10 @@ var execute = function(req, res) {
   if (flds.dob && flds.dob.length > 0) qb.where('dob', otherOp, flds.dob);
   if (flds.doh && flds.doh.length > 0) qb.orWhere('dohID', otherOp, flds.doh);
   if (flds.philHealth && flds.philHealth.length > 0) qb.orWhere('philHealth', otherOp, flds.philHealth);
-  qb.select(cols).then(function(list) {
+  qb
+    .limit(rowsPerPage)
+    .offset((rowsPerPage * (pageNum-1)))
+    .select(cols).then(function(list) {
     _.each(list, function(rec) {
       var r = _.pick(rec, 'id', 'dob', 'dohID', 'firstname', 'lastname', 'address', 'barangay');
       r.dob = moment(r.dob).format('MMM D, YYYY');
@@ -79,6 +100,7 @@ var execute = function(req, res) {
       title: req.gettext('Search Results')
       , user: req.session.user
       , results: results
+      , pageNum: pageNum
     });
   });
 };
