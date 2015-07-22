07046ed0c645881d20c862b746d13160f73b87a6
Kurt Symanzik <kurt@kbsymanzik.org>
2015-07-22 17:20:07 +0800

    Adjustments in historyControl with responsive features.

--- client/js/angular/app.js
+++ client/js/angular/app.js
@@ -127,6 +127,13 @@
       });
     });
 
+  angular.module('midwifeEmr')
+    .run(function() {
+      var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+      var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+      console.log('Width: ' + w + ', Height: ' + h);
+    });
+
   // --------------------------------------------------------
   // Various filters.
   // --------------------------------------------------------
