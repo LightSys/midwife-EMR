b10e4944885c91ed785e15c1a49e33066e55c1af
Kurt Symanzik <kurt@kbsymanzik.org>
2016-04-29 19:00:32 +0800

    Minor visual adjustments.

--- client/js/react/Home/Home.js
+++ client/js/react/Home/Home.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 
 
-const Home = () => {
-  return (
-    <div className='row'>
-      <div className='col-md-12 col-xs-12 col-sm-12 col-lg-12'>
-        <h1>Home</h1>
+class Home extends Component {
+  render() {
+    return (
+      <div className='row'>
+        <div className='col-md-12 col-xs-12 col-sm-12 col-lg-12'>
+          <h1>Home</h1>
+          <p>This page is left intentionally blank.</p>
+          <p><strong>What do you want to see here?</strong></p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Home
