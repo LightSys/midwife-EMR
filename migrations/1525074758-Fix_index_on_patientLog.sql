d8392d7f6b3183cbc8925744d7e6573e1d92ac8f
Kurt Symanzik <kurt@kbsymanzik.org>
2018-05-07 15:17:33 +0800

    Corrected index drop code in migrations.

--- migrations/1525074758-Fix_index_on_patientLog.sql
+++ migrations/1525074758-Fix_index_on_patientLog.sql
@@ -4,7 +4,22 @@
 
 BEGIN;
 
-DROP INDEX `patient_dohid_idx` ON patientLog;
+-- Drop a patientLog index if it exists. This is so that shmig does not
+-- record this as an error if the index does not exist.
+-- Adapted from: https://dba.stackexchange.com/a/62997
+SELECT IF(
+  EXISTS(
+    SELECT DISTINCT index_name FROM information_schema.statistics
+    WHERE table_schema = (SELECT DATABASE())
+    AND table_name = 'patientLog'
+    AND index_name LIKE 'patient_dohid_idx'
+  )
+  , 'DROP INDEX `patient_dohid_idx` ON patientLog;'
+  , 'SELECT ''Unnecessary ... Skipping'' AS ''Drop PatientLog Index'';'
+) INTO @a;
+PREPARE stmt1 FROM @a;
+EXECUTE stmt1;
+DEALLOCATE PREPARE stmt1;
 
 COMMIT;
 
